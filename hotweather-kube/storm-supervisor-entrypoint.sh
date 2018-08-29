b05a430fbbb810bad03a35a0deec2fcdc9f12d51
VulcanoAhab <vulcano.ahab@gmail.com>
2018-08-28 21:15:52 -0300

    Refactor.

--- hotweather-kube/storm-supervisor-entrypoint.sh
+++ hotweather-kube/storm-supervisor-entrypoint.sh
@@ -10,13 +10,13 @@ fi
 
 # Generate the config only if it doesn't exist
 CONFIG="$STORM_CONF_DIR/storm.yaml"
-if [ ! -f "$CONFIG" ]; then
-    cat << EOF > "$CONFIG"
-storm.zookeeper.servers: [zookeeper]
-nimbus.seeds: [nimbus]
+if [ ! -f "$CONFIG" ]; then touch $STORM_CONF_DIR/storm.yaml; fi
+
+cat  >> "$CONFIG" << EOF
+storm.zookeeper.servers: [$ZOOKEEPER]
+nimbus.seeds: [$NIMBUS]
 storm.log.dir: "$STORM_LOG_DIR"
 storm.local.dir: "$STORM_DATA_DIR"
 EOF
-fi
 
 exec "$@"
